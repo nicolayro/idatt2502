caa5ae8fd990ea4a0e0e72beb9e93f1a2a7808d1
Nicolay Roness <nicroness@gmail.com>
2022-08-26 15:22:49 +0200

    Completed task b

--- oving-1/b.py
+++ oving-1/b.py
@@ -3,24 +3,24 @@ import torch
 import matplotlib.pyplot as plt
 
 # Read data from input
-length = []
-weight = []
-with open("./length_weight.csv", 'r') as file:
+x = []
+y = []
+with open("./day_length_weight.csv", 'r') as file:
     reader = csv.reader(file)
     next(reader) # Skip first line
     for row in reader:
-        length.append(float(row[0]))
-        weight.append(float(row[1]))
+        y.append(float(row[0]))
+        x.append([float(row[1]), float(row[2])])
 
 # Observed/training input and output
-x_train = torch.tensor(length).reshape(-1, 1) # x_train = [[1], [1.5], [2], [3], [4], [5], [6]]
-y_train = torch.tensor(weight).reshape(-1, 1) # y_train = [[5], [3.5], [3], [4], [3], [1.5], [2]]
+x_train = torch.tensor(x).reshape(-1, 2)
+y_train = torch.tensor(y).reshape(-1, 1)
 
 class LinearRegressionModel:
 
     def __init__(self):
         # Model variables
-        self.W = torch.tensor([[0.0]], requires_grad=True)  # requires_grad enables calculation of gradients
+        self.W = torch.tensor([[0.0], [0.0]], requires_grad=True) # requires_grad enables calculation of gradients
         self.b = torch.tensor([[0.0]], requires_grad=True)
 
     # Predictor
@@ -37,8 +37,8 @@ model = LinearRegressionModel()
 # Optimize: adjust W and b to minimize loss using stochastic gradient descent
 optimizer = torch.optim.SGD([model.W, model.b], 0.0001, 0.9)
 for epoch in range(100000):
-    model.loss(x_train, y_train).backward()  # Compute loss gradients
-    optimizer.step()  # Perform optimization by adjusting W and b,
+    model.loss(x_train, y_train).backward() # Compute loss gradients
+    optimizer.step() # Perform optimization by adjusting W and b,
     # similar to:
     # model.W -= model.W.grad * 0.01
     # model.b -= model.b.grad * 0.01
@@ -48,13 +48,31 @@ for epoch in range(100000):
 print("W = %s, b = %s, loss = %s" % (model.W, model.b, model.loss(x_train, y_train)))
 
 # Visualize result
-plt.title("Length and weight for babies")
-plt.plot(x_train, y_train, 'o', label='$(x^{(i)},y^{(i)})$')
-plt.xlabel('x')
-plt.ylabel('y')
-x = torch.tensor([[torch.min(x_train)], [torch.max(x_train)]])  # x = [[1], [6]]]
-plt.plot(x, model.f(x).detach(), label='$\\hat y = f(x) = xW+b$')
-plt.legend()
+fig = plt.figure("Age of babies based on length and weight")
+ax = fig.add_subplot(projection="3d")
+
+x_train.sort()
+length = x_train[:, 0].squeeze()
+width = x_train[:, 1].squeeze()
+days = y_train[:, 0].squeeze()
+
+# Plot datapoints
+ax.scatter(length, width, days, label='$(x^{(i)},y^{(i)}),z^{(i)}$')
+
+step = 30
+
+x = torch.linspace(torch.min(length), torch.max(length), step) # Some length values
+y = torch.linspace(torch.min(width), torch.max(width), step) # Some weight values
+z = torch.empty((step, step), dtype=torch.long) # Initialize z values
+
+# Calculate z values based on x and y
+for i, t1 in enumerate(x):
+    for j, t2 in enumerate(y):
+        tensor = torch.tensor([t1, t2])
+        z[i, j] = model.f(tensor).detach()
+
+# Plot plane
+x_grid, y_grid = torch.meshgrid(x, y)
+ax.plot_surface(x_grid, y_grid, z, label='$\\hat y = f(x) = xW+b$', color='yellow', alpha=0.75)
+
 plt.show()
-
-
---
+++ oving-1/c.py
@@ -0,0 +1,17 @@
+from mpl_toolkits.mplot3d import axes3d
+import matplotlib.pyplot as plt
+
+
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+
+# Grab some test data.
+X, Y, Z = axes3d.get_test_data(0.05)
+print(X)
+print(Y)
+print(Z)
+
+# Plot a basic wireframe.
+ax.plot_wireframe(X, Y, Z, rstride=10, cstride=10)
+
+plt.show()
