2d9f78e7fff64311380f148ad984e77c26bb9b94
Nicolay Roness <nicroness@gmail.com>
2022-09-28 13:19:30 +0200

    Completed task 4 and 5

--- oving-4/b copy 2.py
+++ oving-4/b copy 2.py
@@ -11,7 +11,7 @@ class LongShortTermMemoryModel(nn.Module):
         self.lstm = nn.LSTM(encoding_size, 128, batch_first=True)  # 128 is the state size
         self.dense = nn.Linear(128, emoji_size)  # 128 is the state size
 
-    def reset(self, batch_size):  # Reset states prior to new input sequence
+    def reset(self, batch_size = 1):  # Reset states prior to new input sequence
         zero_state = torch.zeros(1, batch_size, 128)  # Shape: (number of layers, batch size, state size)
         self.hidden_state = zero_state
         self.cell_state = zero_state
@@ -29,15 +29,14 @@ class LongShortTermMemoryModel(nn.Module):
 
 # Training data
 
-batch_size = 4
 chars = [' ', 'o', 'g', 'd', 'h', 'l', 'f', 'r']
 emojies = {
     "frog": '🐸', 
     "dog ": '🐶',
 }
+index_to_emoji = ['🐸', '🐶']
 num_chars = len(chars)
 num_emojies = len(emojies)
-index_to_emojies = ['🐸', '🐶']
 
 char_encodings = [[1. if i == j else 0. for j in range(num_chars)] for i in range(num_chars)]
 emoji_encodings = [[1. if i == j else 0. for j in range(num_emojies)] for i in range(num_emojies)]
@@ -46,32 +45,40 @@ def word_to_char_encodings(word):
     return [char_encodings[chars.index(letter)] for letter in word]
 
 def emoji_to_emoji_encodings(emoji):
-    return [emoji_encodings[list(emojies.values()).index(emoji)] for _ in range(batch_size)]
+    return emoji_encodings[list(emojies.values()).index(emoji)]
 
 
 model = LongShortTermMemoryModel(8, 2)
 
+x_train = torch.tensor([word_to_char_encodings('dog '), word_to_char_encodings('frog')])
+print(x_train.shape)
+y_train = torch.tensor([emoji_to_emoji_encodings('🐶'), emoji_to_emoji_encodings('🐸')])
+print(y_train.shape)
+
 optimizer = torch.optim.RMSprop(model.parameters(), 0.001)
-for epoch in range(500):
-    for e in emojies.items():
-        model.reset(4)
-        x_train = torch.tensor(word_to_char_encodings(e[0]))
-        y_train = torch.tensor(emoji_to_emoji_encodings(e[1]))
-        model.loss(x_train, y_train).backward()
-        optimizer.step()
-        optimizer.zero_grad()
+for epoch in range(100):
+    model.reset(2)
+    model.loss(x_train, y_train).backward()
+    optimizer.step()
+    optimizer.zero_grad()
+        
+#rt = rt.reshape(1, 3, encoding_size)
 
 model.reset(1)
-y = model.f(torch.tensor(word_to_char_encodings('dog ')))
-print(y)
-print("'dog ': ", index_to_emojies[y.argmax(1)])
-model.reset()
-y = model.f(torch.tensor(word_to_char_encodings('frog')))
-print("'frog' : ", index_to_emojies[y.argmax(1)])
-model.reset()
-y = model.f(torch.tensor(word_to_char_encodings('frg ')))
-print("'frg' : ", index_to_emojies[y.argmax(1)])
+y = model.f(torch.tensor([word_to_char_encodings('dg ')]))
+print(index_to_emoji[y.argmax(1)])
 
-model.reset()
-y = model.f(torch.tensor(word_to_char_encodings('drog')))
-print("'drog' : ", index_to_emojies[y.argmax(1)])
+model.reset(1)
+y = model.f(torch.tensor([word_to_char_encodings('dogf')]))
+print(y)
+print(index_to_emoji[y.argmax(1)])
+
+model.reset(1)
+y = model.f(torch.tensor([word_to_char_encodings('drog')]))
+print(index_to_emoji[y.argmax(1)])
+# y = model.f(torch.tensor([word_to_char_encodings('dog ')]))
+# print("dogs: ", y)
+# y = model.f(torch.tensor([word_to_char_encodings('frog')]))
+# print("frog : ", y)
+# y = model.f(torch.tensor([word_to_char_encodings('frg ')]))
+# print("frg : ", y)
